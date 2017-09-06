400b871d462b1154eacc2b7121e537680712e74a
Kitty Ng <kittyng89@gmail.com>
2017-09-06 15:31:54 -0500

    working base mode

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,27 @@
 console.log( 'js' );
+
+$(document).ready(onReady);
+
+function onReady() {
+    $('#requestButton').on('click',makeGetRequest);
+    
+    $.ajax ({
+        type : 'GET',
+        url : '/',
+        success: function(res) {
+            console.log('on html load');
+        }
+    });
+};
+
+function makeGetRequest() {
+    console.log('inside make get request');
+
+    $.ajax ({
+        type : 'GET',
+        url : '/dead',
+        success: function(res) {
+            console.log('I am successful!', res);
+        }
+    });
+};
